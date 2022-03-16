aa7ded8874618958715c61d2ab5b761b30d2650e
MephistoRed <101037658+MephistoRed@users.noreply.github.com>
2022-03-16 12:17:46 +0100

    Update and rename feature. to feature.py

---
+++ feature.py
@@ -0,0 +1,3 @@
+var1=int(input("Entrez un entier"))
+var2=var1/2
+print("la moitié de cet entier est :", var2 )
--- feature.sh
+++
@@ -1,2 +0,0 @@
-var1=int(input("Entrez un entier"))
-print("la moitié de cet entier est :", var1 )
